3e0dd8cdff2e48d24dac0a2df668d1847a4f7c62
mynameis-nikita <nick.kozhevatov@gmail.com>
2023-06-28 22:54:18 +0700

    fixed task_4

--- wib_task4.sql
+++ wib_task4.sql
@@ -3,22 +3,20 @@
 Найти топ-3 товаров по выручке и их доля в общей выручке за любой год 
 
 Найду топ-3 за 2022. 
-Решение с использованием переменной и оконной функции ранжирования.
+Решение с оконной функцией ранжирования.
 Если какие-то товары наберут одинаковую сумму продаж в топе, то они также попадут в топ.
 */
 
 
-SET @total := (SELECT SUM(price) 
-			   FROM Purchases INNER JOIN Items USING (itemId) 
-		       WHERE YEAR(Purchases.`date`) = 2022)
 
-
-SELECT itemId, summa, summa/@total*100 AS part, rnk
-FROM (SELECT itemId, SUM(price) as summa, RANK() OVER (ORDER BY SUM(price)) AS rnk
+SELECT rnk, itemId, summa, 
+       summa * 100 / (SELECT SUM(price) 
+					  FROM Purchases INNER JOIN Items USING (itemId) 
+		       	      WHERE YEAR(Purchases.`date`) = 2022) AS part 
+	   
+FROM (SELECT itemId, SUM(price) as summa, RANK() OVER (ORDER BY SUM(price) DESC) AS rnk
 	  FROM Purchases INNER JOIN Items USING(itemID)
 	  WHERE YEAR(Purchases.`date`) = 2022
 	  GROUP BY itemId) t1
 WHERE rnk <= 3
-		
-		
 
